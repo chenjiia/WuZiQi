03e642cc2dd7af2b68b7976a14d73bfed79f59fe
Jia Chen <jckg7@mail.missouri.edu>
2014-07-22 15:19:28 -0500

    Remove account related files

--- account/account_validator.php
+++
@@ -1,8 +0,0 @@
-<?php
-//This file contains only functions
-
-//'username' has to be alphanumeric
-function validator_username($password){
-	return !preg_match('/[^a-zA-Z0-9]/', $password) && !empty($password);
-}
-?>
